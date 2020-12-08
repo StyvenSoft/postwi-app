ca04fa8302ba5c0e0e745222eff263352c0b547b
Steveen <steveen.echeverri@gmail.com>
2020-12-08 15:45:42 -0500

    User/Post relationship and create post

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -6,8 +6,25 @@ use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
-    public function idex()
+    public function index()
     {
         return view('posts.index');
+    }
+
+    public function store(Request $request)
+    {
+        // dd('ok');
+        $this->validate($request, [
+            'body' => 'required'
+        ]);
+
+        // Post::create([
+        //     'user_id' => auth()->id(),
+        //     'body' => $request->body
+        // ]);
+
+        $request->user()->posts()->create($request->only('body'));
+
+        return back();
     }
 }
