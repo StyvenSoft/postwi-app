0a78dd9f323cc9882ecc5343652be670ad46913c
Steveen <steveen.echeverri@gmail.com>
2020-12-11 15:13:09 -0500

    Improving authorization policies

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -10,7 +10,7 @@ class PostController extends Controller
     public function index()
     {
 
-        $posts = Post::with(['user', 'likes'])->paginate(10);
+        $posts = Post::latest()->with(['user', 'likes'])->paginate(10);
 
         return view('posts.index', [
             'posts' => $posts
@@ -36,6 +36,8 @@ class PostController extends Controller
 
     public function destroy(Post $post)
     {
+        $this->authorize('delete', $post);
+
         $post->delete();
 
         return back();
